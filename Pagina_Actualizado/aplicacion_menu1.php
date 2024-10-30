8c5a928a7758dc22a809d44d22b526c76170e11a
aldoxp <alfredopetul102@gmail.com>
2024-10-30 10:46:33 -0600

    Delete Pagina_Actualizado directory

--- Pagina_Actualizado/aplicacion_menu1.php
+++
@@ -1,8 +0,0 @@
-<nav class="nav flex-column">
-    <a class="nav-link active" href="#">Inicio</a>
-    <a class="nav-link" href="#">Usuarios</a>
-    <a class="nav-link" href="#">Pedidos</a>
-    <a class="nav-link" href="#">Productos</a>
-    <a class="nav-link" href="#">Reportes</a>
-    <a class="nav-link" href="#">Configuración</a>
-</nav>
--- Pagina_Actualizado/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start(); // Inicia la sesión
-session_destroy(); // Destruye la sesión
-header("Location: login.php"); // Redirige al login
-exit();
-?>
